87e4a7a3394aba81ca4df9334b6bd42c14319524
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-09 12:58:08 +0330

    Ready To Get Seeded

--- ReviewerApp/Program.cs
+++ ReviewerApp/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using ReviewerApp.Data;
+
 namespace ReviewerApp
 {
     public class Program
@@ -13,6 +16,14 @@ namespace ReviewerApp
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+                );
+
+
+
 
             var app = builder.Build();
 
