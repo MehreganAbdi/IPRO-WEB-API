c1fd35ed69562cb4427e36a250b10f080791d3af
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-12 20:48:29 +0330

    Pokeman DTO Added / Ready For Dara Access Changes

---
+++ ReviewerApp/DTOs/PokemanDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ReviewerApp.DTOs
+{
+    public class PokemanDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
