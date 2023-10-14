dea4a03e37b15f1979702d624288f3489884bfbf
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-14 10:40:44 +0330

    Category Repo Added

---
+++ ReviewerApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewerApp.Models;
+
+namespace ReviewerApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategoryById(int Id);
+        ICollection<Pokeman> GetPokemansByCategory(int CategoryId);
+        bool CategoryExists(int Id);
+        
+    }
+}
---
+++ ReviewerApp/Repository/CategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using ReviewerApp.Data;
+using ReviewerApp.Interfaces;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExists(int Id)
+        {
+            return _context.Categories.Any(c => c.Id == Id);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int Id)
+        {
+            return _context.Categories.FirstOrDefault(c => c.Id == Id);
+        }
+
+        public ICollection<Pokeman> GetPokemansByCategory(int CategoryId)
+        {
+            return _context.PokemanCategories.Where(c=>c.CategoryId == CategoryId).Select(p=>p.Pokeman).ToList();
+        }
+    }
+}
