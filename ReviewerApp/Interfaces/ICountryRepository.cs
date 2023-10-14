c2975c7ac46741f8114e41eb83a10bd3f65dc4d0
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-14 12:10:29 +0330

    Country Repo Added

---
+++ ReviewerApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewerApp.Models;
+
+namespace ReviewerApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetAllCountries();
+        Country GetCountryById(int Id);
+        Country GetCountryByOwnerId(int ownerId);
+        bool CountryExists(int Id);
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+    }
+}
---
+++ ReviewerApp/Repository/CountryRepository.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ReviewerApp.Data;
+using ReviewerApp.Interfaces;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _context;
+
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CountryExists(int Id)
+        {
+            return _context.Countries.Any(c => c.Id == Id);
+        }
+
+        public ICollection<Country> GetAllCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountryById(int Id)
+        {
+            return _context.Countries.FirstOrDefault(c => c.Id == Id);
+        }
+
+        public Country GetCountryByOwnerId(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).Select(r => r.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromACountry(int countryId)
+        {
+            return _context.Owners.Where(c =>c.Country.Id == countryId).ToList();
+        }
+    }
+}
