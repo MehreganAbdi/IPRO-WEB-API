a81b41961b072491905d905dbb36d56a07d0fbb6
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-14 12:16:38 +0330

    Ready To Add Country Controller

---
+++ ReviewerApp/Controllers/CountryController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using ReviewerApp.Interfaces;
+
+namespace ReviewerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public CountryController(ICountryRepository countryRepository , IMapper mapper)
+        {
+            _countryRepository = countryRepository;
+            _mapper = mapper;
+        }
+
+
+    }
+}
