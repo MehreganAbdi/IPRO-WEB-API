7c44a9236d0939960d0ec2d0c929b70dd944d945
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-14 10:07:24 +0330

    AutoMapper Added

---
+++ ReviewerApp/Helper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReviewerApp.DTOs;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Helper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Pokeman, PokemanDTO>();
+        }
+    }
+}
