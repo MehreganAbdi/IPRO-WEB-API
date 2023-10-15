408ffe0e17ba41954c367ce81d130f67efc9414a
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-15 14:31:28 +0330

    OwnerInterface Added

---
+++ ReviewerApp/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewerApp.Models;
+
+namespace ReviewerApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int Id);
+        ICollection<Owner> GetOwnerOfAPokeman(int pokemanId);
+        ICollection<Pokeman> GetPokemanByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+    }
+}
