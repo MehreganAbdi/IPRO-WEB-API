7dda4466ea020e18d2983dea0453ac17f9ffff52
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-15 14:58:14 +0330

    OwnerRepo Implemented

---
+++ ReviewerApp/Repository/OwnerRepository.cs
@@ -0,0 +1,40 @@
+﻿using ReviewerApp.Data;
+using ReviewerApp.Interfaces;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Owner GetOwner(int Id)
+        {
+            return _context.Owners.FirstOrDefault(o => o.Id == Id);
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokeman(int pokemanId)
+        {
+            return _context.PokemanOwners.Where(p => p.PokemanId == pokemanId).Select(o => o.Owner).ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public ICollection<Pokeman> GetPokemanByOwner(int ownerId)
+        {
+            return _context.PokemanOwners.Where(p => p.Owner.Id == ownerId).Select(u => u.Pokeman).ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
