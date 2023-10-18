b0a9d9fe1cee87ccb520a6e6f2ac87c4c149c9c5
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-18 12:56:25 +0330

    Reviwer Repo Added

---
+++ ReviewerApp/Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReviewerApp.Models;
+
+namespace ReviewerApp.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetAllReviewers();
+        Reviewer GetReviewerById(int Id);
+        ICollection<Review> GetReviewsByReviwer(int ReviewerId);
+        bool ReviwerExists(int Id);
+    }
+}
---
+++ ReviewerApp/Repository/ReviwerRepository.cs
@@ -0,0 +1,35 @@
+﻿using ReviewerApp.Data;
+using ReviewerApp.Interfaces;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Repository
+{
+    public class ReviwerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        public ReviwerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Reviewer> GetAllReviewers()
+        {
+            return _context.Reviewers.ToList();
+
+        }
+
+        public Reviewer GetReviewerById(int Id)
+        {
+            return _context.Reviewers.FirstOrDefault(r => r.Id == Id);
+        }
+
+        public ICollection<Review> GetReviewsByReviwer(int ReviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == ReviewerId).ToList();
+        }
+
+        public bool ReviwerExists(int Id)
+        {
+            return _context.Reviews.Any(r => r.Id == Id);
+        }
+    }
+}
