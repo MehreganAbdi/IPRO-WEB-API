f850e397b6ce8160bc52f0c93d9f6772415120ee
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-18 12:44:45 +0330

    Review Repo Added

---
+++ ReviewerApp/Interfaces/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReviewerApp.Models;
+
+namespace ReviewerApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetAllReview();
+        Review GetReviewById(int Id);
+        bool ReviewExists(int Id);
+        ICollection<Review> GetReviewsOfPokeman(int pokeId);
+
+    }
+}
---
+++ ReviewerApp/Repository/ReviewRepository.cs
@@ -0,0 +1,34 @@
+﻿using ReviewerApp.Data;
+using ReviewerApp.Interfaces;
+using ReviewerApp.Models;
+
+namespace ReviewerApp.Repository
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly DataContext _context;
+        public ReviewRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Review> GetAllReview()
+        {
+            return _context.Reviews.ToList();
+        }
+
+        public Review GetReviewById(int Id)
+        {
+            return _context.Reviews.FirstOrDefault(r => r.Id == Id);
+        }
+
+        public ICollection<Review> GetReviewsOfPokeman(int pokeId)
+        {
+            return _context.Reviews.Where(r => r.Pokeman.Id == pokeId).ToList();
+        }
+
+        public bool ReviewExists(int Id)
+        {
+            return _context.Reviews.Any(r => r.Id == Id);
+        }
+    }
+}
